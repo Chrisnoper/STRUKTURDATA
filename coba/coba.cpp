9a0a466ddef702cc61f004b020182aad0556199f
Chrisnoper <boychrisnoper@student.esaunggul.ac.id>
2025-05-14 11:37:41 +0700

    nambah

---
+++ coba/coba.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "halo boy";
+
+    cout << nama << endl;
+    return 0;
+}
---
+++ cobaco/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+    string nama = "hello world";
+
+    cout << "HELLO WORLD" << endl;
+    return 0;
+}
